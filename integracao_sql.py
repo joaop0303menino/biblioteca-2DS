6b4045a75b0054033e7a83c163f855d8bc2a5d81
João Pedro Almeida Menino <joaopedroalmeidamenino@gmail.com>
2024-07-09 16:08:37 -0300

    terminei de criar as tabelas

--- integracao_sql.py
+++ integracao_sql.py
@@ -1,16 +1,2 @@
-import pyodbc 
-
-def  juncao_sql():
-    dados_conexao = (
-        'DRIVER={SQL Server};'
-        'Server=DESKTOP-JHQABHK;'
-        'DATABASE=dados da biblioteca;'
-    )
-
-    conexao = pyodbc.connect(dados_conexao)
-    cursor = conexao.cursor()
-    return cursor, conexao
-    print('Conexão sucedida')
-    
 
 
