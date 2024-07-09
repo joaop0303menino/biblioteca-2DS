af02bf39998f6e82f1b4b36d879a61a66f297504
João Pedro Almeida Menino <joaopedroalmeidamenino@gmail.com>
2024-07-09 16:01:31 -0300

    Criando a tabela usuário do banco de dados

--- dados_biblioteca.sql
+++ dados_biblioteca.sql
@@ -6,34 +6,3 @@ CREATE TABLE administrador(
 	senha varchar(20) not null
 )
 
-CREATE TABLE usuario(
-	id int primary key,
-	id_administrador int foreign key references administrador(id),
-	nome varchar(50) not null,
-	sobrenome varchar(50) not null
-)
-
-CREATE TABLE livro(
-	codigo int primary key,
-	id_usuario int foreign key references usuario(id),
-	nome varchar(150) not null,
-	quantidade int not null 
-)
-
-CREATE TABLE aluno(
-	RA varchar(50) primary key ,
-	id_usuario int foreign key references usuario(id),
-	codigo_livro int foreign key references livro(codigo),
-	nome varchar(50) not null,
-	sobrenome varchar(50) not null,
-	serie varchar(50) not null
-)
-
-CREATE TABLE historico(
-	RA_aluno varchar(50) primary key foreign key references aluno(RA),
-	codigo_livro int foreign key references livro(codigo),
-	dataRetirada datetime not null,
-	dataDevolucao datetime not null, 
-	estado varchar(10) not null,
-	check (estado in ('pendente', 'entregue'))
-)
