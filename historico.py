8329160aa20d13cdf8c2bb5d55cc4343d9b4f104
João Pedro Almeida Menino <joaopedroalmeidamenino@gmail.com>
2024-07-09 16:21:44 -0300

    CRUD Função atualizar histórico concluída com sucesso

--- historico.py
+++ historico.py
@@ -41,3 +41,30 @@ def inserir_historico(datetime):
             cursor.commit()    
     
         ver_historico()
+        
+def Atualizar_historico():
+    
+    ver_historico()
+    
+    print("Digite o número da opção que deseja alterar")
+    print(f"1) Ra do aluno\n 2) Código do livro\n 3) estado")
+    escolha = int(input("qual opção você deseja: "))
+    
+    opcao = ''
+    mudanca = input(f"Digite a sua Alteração: ")
+    Oq_mudar = input(f"Digite o RA do aluno que deseja alterar: ")
+    
+    if escolha == 1:
+        opcao = 'RA_aluno'
+    elif escolha == 2:
+        opcao = 'codigo_livro'
+    elif escolha == 3: 
+        opcao = 'estado'
+    else:
+        print("Opção inválida tente novamente")
+        Atualizar_historico() 
+        
+    cursor.execute(f"update historico set {opcao} = ? where codigo = ?",(mudanca, Oq_mudar))
+    cursor.commit()  
+    
+    ver_historico()
