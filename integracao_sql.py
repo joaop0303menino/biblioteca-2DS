7eb89cd219e7f63d167caf62f2984112616ecbc6
João Pedro Almeida Menino <joaopedroalmeidamenino@gmail.com>
2024-07-09 16:11:48 -0300

    Integração com SQL

--- integracao_sql.py
+++ integracao_sql.py
@@ -1,2 +1,12 @@
+import pyodbc 
 
+def  juncao_sql():
+    dados_conexao = (
+        'DRIVER={SQL Server};'
+        'Server=DESKTOP-JHQABHK;'
+        'DATABASE=dados da biblioteca;'
+    )
 
+    conexao = pyodbc.connect(dados_conexao)
+    cursor = conexao.cursor()
+    return cursor, conexao
