293771fc791fb8191fbea98d4c05a789c61d8693
João Pedro Almeida Menino <joaopedroalmeidamenino@gmail.com>
2024-07-10 15:21:16 -0300

    Função CRUD Criar administrador concluído com sucesso

--- adm.py
+++ adm.py
@@ -10,3 +10,22 @@ def ver_administrador():
     tabela = pd.read_sql(f'select * from administrador',conexao)  
     print(tabela)
 
+def inserir_administrador():
+    
+    print(f"pressione enter para continuar \npara sair digite '!n'")
+    dados = ""
+    
+    while dados != "!n":
+        dados = input('Deseja continuar: ')
+        
+        if dados != "!n":
+        
+            id = int(input(f'Digite o código do administrador: '))
+            login = input(f'Digite o login do administrador: ')
+            senha = input(f'Digite a senha de administrador: ')
+            senha_criptografada = sha256(senha.encode()).hexdigest()
+            
+            cursor.execute(f"insert into administrador(id,login,senha) values (?,?,?)",(id,login,senha_criptografada))
+            cursor.commit()  
+        else:   
+            ver_administrador()
