81222fbe9bb5637617a352878f84cbe24c10b29c
João Pedro Almeida Menino <joaopedroalmeidamenino@gmail.com>
2024-07-10 12:37:02 -0300

    Função CRUD Criar usuários concluído com sucesso

--- usuario.py
+++ usuario.py
@@ -9,3 +9,22 @@ def ver_usuario():
     
     tabela = pd.read_sql(f'select * from usuario',conexao)  
     print(tabela)
+    
+def inserir_usuario():
+    
+    print(f"pressione enter para continuar \npara sair digite '!n'")
+    dados = ""
+    
+    while dados != "!n":
+        dados = input('Deseja continuar: ')
+        
+        if dados != "!n":
+        
+            id = int(input(f'Digite o código do usuario: '))
+            nome = input(f'Digite o nome do usuario: ')
+            sobrenome = input(f'Digite a sobrenome de usuario: ')
+            
+            cursor.execute(f"insert into usuario(id,nome,sobrenome) values ({id},'{nome}','{sobrenome}')")
+            cursor.commit()  
+        else:   
+            ver_usuario()
