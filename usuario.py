d29838d44866cb670a53dd624d2b7312d799c932
João Pedro Almeida Menino <joaopedroalmeidamenino@gmail.com>
2024-07-10 13:04:42 -0300

    Função CRUD Atualizar usuários concluído com sucesso

--- usuario.py
+++ usuario.py
@@ -28,3 +28,30 @@ def inserir_usuario():
             cursor.commit()  
         else:   
             ver_usuario()
+
+def atualizar_usuario():
+    
+    ver_usuario()
+    
+    print("Digite o número da opção que deseja alterar")
+    print(f"1) ID do usuario\n 2) Nome do usuario\n 3) sobrenome de usuario")
+    escolha = int(input("qual opção você deseja: "))
+    
+    opcao = ''
+    mudanca = input(f"Digite a sua Alteração: ")
+    Oq_mudar = input(f"Digite o ID do usuario que deseja alterar: ")
+    
+    if escolha == 1:
+        opcao = 'id'
+    elif escolha == 2:
+        opcao = 'nome'
+    elif escolha == 3: 
+        opcao = 'sobrenome'
+    else:
+        print("Opção inválida tente novamente")
+        atualizar_usuario() 
+        
+    cursor.execute(f"update usuario set {opcao} = ? where id = ?",(mudanca, Oq_mudar))
+    cursor.commit()  
+    
+    ver_usuario()
