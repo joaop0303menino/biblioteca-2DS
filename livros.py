3992441ed3dd2cb3f94b1c4fcecc45b052d49e83
João Pedro Almeida Menino <joaopedroalmeidamenino@gmail.com>
2024-07-10 11:03:28 -0300

    Função CRUD Criar livros concluído com sucesso

--- livros.py
+++ livros.py
@@ -9,5 +9,22 @@ def ver_livros():
     
     tabela = pd.read_sql(f'select * from livro',conexao)  
     print(tabela)
-
-ver_livros()
+  
+def inserir_livros():
+    
+    print(f"pressione enter para continuar \npara sair digite '!n'")
+    dados = ""
+    
+    while dados != "!n":
+        dados = input('Deseja continuar: ')
+        
+        if dados != "!n":
+        
+            codigo = int(input(f'Digite o código do livro: '))
+            nome = input(f'Digite o nome do livro: ')
+            quantidade = int(input(f'Digite a quantidade de livros: '))
+            
+            cursor.execute(f"insert into livro(codigo,nome,quantidade) values ({codigo},'{nome}',{quantidade})")
+            cursor.commit()  
+        else:   
+            ver_livros()
