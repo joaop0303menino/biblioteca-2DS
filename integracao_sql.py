cc481216a6213ffa98dc26010ca55c7afe15f723
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-07-15 18:15:43 -0300

    conexão do banco de dados pronta

--- integracao_sql.py
+++ integracao_sql.py
@@ -1,12 +1,19 @@
-import pyodbc 
+import mysql.connector
 
-def  juncao_sql():
-    dados_conexao = (
-        'DRIVER={SQL Server};'
-        'Server=Bruno;'
-        'DATABASE=dados da biblioteca;'
+def juncao_sql():
+    mydb = mysql.connector.connect(
+        host="149.100.155.154",
+        user="u895973460_carlos_gomes",
+        password="123456789Carlos_gomes",
+        database="u895973460_Biblioteca"
+
     )
 
-    conexao = pyodbc.connect(dados_conexao)
-    cursor = conexao.cursor()
-    return cursor, conexao
+    
+    try:
+        print('Conexão bem sucedida')
+        cursor = mydb.cursor()
+        return cursor, mydb
+    except:
+        return "Erro na conexão"
+
